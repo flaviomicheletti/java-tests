201145ec73d3f606206c447919bd8add5e96bc50
Flávio Micheletti <famicheletti@indracompany.com>
2019-05-17 14:01:03 -0300

    Foo

---
+++ src/test/java/com/example/project/FooTest.java
@@ -0,0 +1,18 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class FooTest {
+
+    @Test
+    public void firstTest(){
+        String expected = "my-test";
+
+        Foo obj = new Foo();
+        String actual = obj.lish();
+
+        assertEquals(expected, actual);
+    }
+
+}
