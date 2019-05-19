2ba497be9750abe525ef0541de2c524ae8014ba3
Flávio Micheletti <famicheletti@indracompany.com>
2019-05-19 12:52:42 -0300

    utils.atomic

---
+++ src/test/java/com/example/project/Utils.java
@@ -0,0 +1,20 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Utils {
+
+    @Test
+    public void atomic(){
+    	
+    	// https://spring.io/guides/gs/intellij-idea/
+		// https://github.com/spring-guides/gs-rest-service    	
+        AtomicLong counter = new AtomicLong();
+        
+        assertEquals(1, counter.incrementAndGet());
+        assertEquals(2, counter.incrementAndGet());
+        assertEquals(3, counter.incrementAndGet());
+    }	
+}
