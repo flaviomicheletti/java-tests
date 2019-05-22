0bd8f7af42f3f73b605e38531c3bef67fba4a6be
Flávio Micheletti <famicheletti@indracompany.com>
2019-05-22 15:09:23 -0300

    Generics

--- src/test/java/com/example/project/GenericsTest.java
+++ src/test/java/com/example/project/GenericsTest.java
@@ -1,48 +1,66 @@
 package com.example.project;
 
 import org.junit.jupiter.api.Test;
-//import java.util.ArrayList;
-//import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //
 //https://www.tutorialspoint.com/java/java_generics.htm
 //
 class GenericsTest {
 	
-    public static < E > void printArray( E[] inputArray )
+    public static < E > int getLength( E[] arr )
     {
-        for(E element : inputArray)
-        {
-            System.out.printf("%s ", element);
-        }
-        System.out.println();
+    	
+//        for(E value : arr)
+//        {
+//            System.out.printf("%s ", value);
+//        }
+//        System.out.println();
+        
+        return arr.length;
     }
 	
 
 	@Test
-	void test() {
-        Integer[] intArray    = { 1, 2, 3, 4, 5 };
+	void firstTest() {
+        Integer[] intArray    = { 1, 2, 3 };
         Double[] doubleArray  = { 1.1, 2.2, 3.3, 4.4 };
         Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
 
-        System.out.println("Array integerArray contains:");
-        printArray(intArray);
-
-        System.out.println("\nArray doubleArray contains:");
-        printArray(doubleArray);
-
-        System.out.println("\nArray characterArray contains:");
-        printArray(charArray);
-
-
-
-        // List list = new ArrayList();
-        // list.add("hello");
-        // String s = (String) list.get(0); // cast
-
-        // List<String> list = new ArrayList<String>();
-        // list.add("hello");
-        // String s = list.get(0);   // no cast
+        assertEquals(3, getLength(intArray));
+        assertEquals(4, getLength(doubleArray));
+        assertEquals(5, getLength(charArray));
+                
     }
+
+    @Test
+    void casting() {
+
+      List list1 = new ArrayList();
+      list1.add("hello");
+      String s1 = (String) list1.get(0); // cast
+
+      List<String> list2 = new ArrayList<String>();
+      list2.add("hello");
+      String s2 = list2.get(0);   // no cast
+
+    }	
+	
+    //
+    // https://www.devmedia.com.br/java-generics-trabalhando-com-metodos/30911
+    //
+    @Test
+    void listCOmeSemGenerics() {
+
+    	Generics obj = new Generics();
+        assertEquals(6, obj.listComGenerics());
+        assertEquals(6, obj.listSemGenerics());
+
+    }
+
 
 }
