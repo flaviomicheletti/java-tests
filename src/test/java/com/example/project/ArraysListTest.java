70f845c8bb5dd78e1174575dcb0109b073299eb0
Flávio Micheletti <famicheletti@indracompany.com>
2019-05-23 14:32:16 -0300

    listInterface()

--- src/test/java/com/example/project/ArraysListTest.java
+++ src/test/java/com/example/project/ArraysListTest.java
@@ -1,14 +1,36 @@
 package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /*
  * https://docs.oracle.com/javase/10/docs/api/java/util/ArrayList.html
  */
 public class ArraysListTest {
    
+	//
+	// O List é uma interface e o ArrayList é a classe que a implementa
+	//
+	@Test
+    public void listInterface(){
+        List arr, linked;
+        
+        arr    = new ArrayList();
+        linked = new LinkedList();
+
+        assertEquals("class java.util.ArrayList", arr.getClass().toString());
+        assertTrue(arr instanceof List);
+        assertTrue(arr instanceof ArrayList);
+
+        assertEquals("class java.util.LinkedList", linked.getClass().toString());
+        assertTrue(linked instanceof List);
+        assertTrue(linked instanceof LinkedList);
+
+    }  	
 
 	/*
 	 * https://pt.stackoverflow.com/questions/11740/como-criar-um-array-infinito
@@ -27,3 +49,4 @@ public class ArraysListTest {
     }     
     
 }
+
