97224a2f2976240ad2b112021b6f25017ea687ba
Flávio Micheletti <famicheletti@indracompany.com>
2019-05-23 23:50:24 -0300

    Vector

---
+++ src/test/java/com/example/project/VectorTest.java
@@ -0,0 +1,26 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import java.util.Vector;
+
+public class VectorTest {
+
+    @Test
+    public void criando(){
+
+        /* Vector of initial capacity(size) of 2 */
+        Vector<String> vec = new Vector<String>(2);
+        assertEquals(2, vec.capacity());
+
+        /* Adding elements to a vector*/
+        vec.addElement("Apple");
+        vec.addElement("Orange");
+        vec.addElement("Mango");
+        vec.addElement("Fig");
+
+        assertEquals(4, vec.size());
+        assertEquals(4, vec.capacity());
+
+    }
+}
