c475771a73c5090cd56ba7f3aab979cacbe11f84
Flávio Micheletti <famicheletti@indracompany.com>
2019-05-23 23:50:11 -0300

    StringBuilder

---
+++ src/test/java/com/example/project/StringBuilderTest.java
@@ -0,0 +1,21 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class StringBuilderTest {
+
+    @Test
+    public void concatenando(){
+        StringBuilder str1 = new StringBuilder("Flavio");
+        str1.append(" Alexandre");
+        str1.append(" Micheletti");
+    	assertEquals("Flavio Alexandre Micheletti", str1.toString());
+
+        StringBuilder str2 = new StringBuilder("Flavio");
+        str2.append(" Alexandre").append(" Micheletti");
+    	assertEquals("Flavio Alexandre Micheletti", str2.toString());
+
+
+    }
+}
