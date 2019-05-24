02a3ea2a93570095517d4ae1e4fb1cbfbc5406ed
Flávio Micheletti <famicheletti@indracompany.com>
2019-05-23 23:50:16 -0300

    String

---
+++ src/test/java/com/example/project/StringTest.java
@@ -0,0 +1,39 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    public void concatenando(){
+
+        String s  = "";
+        int times = 3;
+
+        for (int i = 0; i < times; i++) {
+            s += "abc-";
+        }
+    	assertEquals("abc-abc-abc-", s);
+
+    }
+
+    //
+    // https://pt.stackoverflow.com/a/71518
+    //
+    @Test
+    public void concatenandob(){
+
+        StringBuilder s = new StringBuilder();
+        int times = 3;
+
+        for (int i = 0; i < times; i++) {
+            s.append("abc-");
+        }
+    	assertEquals("abc-abc-abc-", s.toString());
+
+    }
+
+}
