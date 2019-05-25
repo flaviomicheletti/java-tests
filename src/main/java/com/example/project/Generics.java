ff88714bbcb9c239f6f9ad594f33360a0be6e13c
Flávio Micheletti <famicheletti@indracompany.com>
2019-05-25 19:31:31 -0300

    Generics

--- src/main/java/com/example/project/Generics.java
+++ src/main/java/com/example/project/Generics.java
@@ -6,34 +6,34 @@ import java.util.List;
 public class Generics {
 
     public int listComGenerics(){
-    	
+
         List<Integer> lista = new ArrayList<Integer>();
         lista.add(1);
         lista.add(2);
         lista.add(3);
-         
+
         int total = 0;
         for(Integer i : lista){
                total += i;
         }
-         
-        return total;	
-    }	
-	
-    public int listSemGenerics(){            
-        
+
+        return total;
+    }
+
+    public int listSemGenerics(){
+
         List lista = new ArrayList();
         lista.add(1);
         lista.add(2);
         lista.add(3);
-         
+
         int total = 0;
         for (int i = 0; i < lista.size(); i++){
                total += (int) lista.get(i);
         }
-         
-        return total;	
-             
-      }    
-    
+
+        return total;
+
+      }
+
 }
