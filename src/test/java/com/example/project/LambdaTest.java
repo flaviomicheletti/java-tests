09b03a88786d5d22611af0a53fb8eee1fd5d9c89
Flávio Micheletti <flaviomicheletti@hotmail.com>
2019-08-18 18:29:47 -0300

    uma lambda a mais

--- src/test/java/com/example/project/LambdaTest.java
+++ src/test/java/com/example/project/LambdaTest.java
@@ -4,17 +4,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 
-interface myLambdaFunction {
+interface myLambdaFunction1 {
     String call();
+}
+
+interface myLambdaFunction2 {
+    int call(int x);
 }
 
 
 public class LambdaTest {
 
+	
     @Test
-    public void lambdaHelloWorldTest(){
-    	myLambdaFunction lamb = () -> "Hello world";
+    public void lambda01Test(){
+    	myLambdaFunction1 lamb = () -> "Hello world";
         assertEquals("Hello world", lamb.call());
+    }
+
+    @Test
+    public void lambda02Test(){
+    	myLambdaFunction2 lamb = (int x) -> 2 * x;
+        assertEquals(20, lamb.call(10));
     }
 
 }
